66af1ddb106d9546bc0aea5ba5059b539af9d611
Mohan Mani Kumar <125678766+ManiKumar830@users.noreply.github.com>
2023-08-02 00:00:59 +0530

    Update index.js

--- src/components/AdminPage/AdminAllTransactions/index.js
+++ src/components/AdminPage/AdminAllTransactions/index.js
@@ -1,9 +1,11 @@
 import {Component} from 'react'
-import {Link} from 'react-router-dom'
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {v4} from 'uuid'
-import DetailsPage from '../DetailsPages'
+
 import TransactionItem from '../TransactionItem'
+import AdminSideBar from '../AdminSideBar'
 
 import './index.css'
 
@@ -14,35 +16,8 @@ const apiStatusConstants = {
   inProgress: 'IN_PROGRESS',
 }
 
-const allPages = [
-  {
-    id: 1,
-    BlueImageUrl: 'https://hmp.me/d6r2',
-    NormalImageUrl: 'https://hmp.me/d6u3',
-    text: 'Dashboard',
-    link: '/admin/dashboard',
-  },
-
-  {
-    id: 2,
-    BlueImageUrl: 'https://hmp.me/d6u6',
-    NormalImageUrl: 'https://hmp.me/d6u5',
-    text: 'All Transactions',
-    link: '/admin/transactions',
-  },
-
-  {
-    id: 3,
-    BlueImageUrl: 'https://hmp.me/d6u8',
-    NormalImageUrl: 'https://hmp.me/d6u7',
-    text: 'Profile',
-    link: '/admin/profile',
-  },
-]
-
 class AdminAllTransactions extends Component {
   state = {
-    activeId: allPages[1].id,
     allTransactionsDetails: [],
     apiStatus: apiStatusConstants.initial,
   }
@@ -52,6 +27,10 @@ class AdminAllTransactions extends Component {
   }
 
   getDetails = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+
     const url =
       'https://bursting-gelding-24.hasura.app/api/rest/daywise-totals-last-7-days-admin'
     const options = {
@@ -84,86 +63,89 @@ class AdminAllTransactions extends Component {
         allTransactionsDetails: newData,
         apiStatus: apiStatusConstants.success,
       })
-    } else {
-      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+    if (response.status === 404) {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
     }
   }
 
-  onChangePageView = id => {
-    this.setState({activeId: id})
+  onClickLogOut = () => {
+    const {history} = this.props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  renderLoadingView = () => (
+    <div className="products-details-loader-container" data-testid="loader">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  renderFailureView = () => (
+    <div className="product-details-error-view-container">
+      <img
+        alt="error view"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+        className="error-view-image"
+      />
+      <h1 className="product-not-found-heading">Not Found</h1>
+    </div>
+  )
+
+  renderTransactionView = () => {
+    const {allTransactionsDetails} = this.state
+
+    return (
+      <div>
+        <div className="Transaction-Header-container-1">
+          <h1 className="accounts-text">Transactions</h1>
+        </div>
+
+        <div className="all-transactions-ul-el">
+          <div className="transaction-heading-container">
+            <p className="userName-text ut">UserName</p>
+            <p className="userName-text Transaction-Name">Transaction Name</p>
+            <p className="userName-text Category-text">Category</p>
+            <p className="userName-text date-text">Date</p>
+            <p className="userName-text amount-text">Amount</p>
+          </div>
+          <ul>
+            {allTransactionsDetails.map(eachItem => (
+              <TransactionItem
+                key={eachItem.id}
+                transactionDetails={eachItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  renderAllTransactionView = () => {
+    const {apiStatus} = this.state
+    console.log(apiStatus)
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderTransactionView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
   }
 
   render() {
-    const {activeId, allTransactionsDetails} = this.state
     return (
       <div className="bg-container-2">
-        <div className="left-side-container">
-          <div>
-            <div className="logo-text-container">
-              <img className="logo" alt="logo" src="https://hmp.me/d6r0" />
-              <h1 className="money-matters-text">
-                Money <span className="matter-text">Matters</span>
-              </h1>
-            </div>
+        <AdminSideBar />
 
-            <div>
-              <ul className="ul-list">
-                {allPages.map(eachItem => (
-                  <DetailsPage
-                    isActive={activeId === eachItem.id}
-                    key={eachItem.text}
-                    pageDetails={eachItem}
-                    onChangePageView={this.onChangePageView}
-                  />
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="logout-container">
-            <img
-              className="profile-image"
-              alt="avatar"
-              src="https://hmp.me/d6vf"
-            />
-            <div className="profile-text-container">
-              <p className="user-name">Rhye</p>
-              <p className="gmail-text">olivia@untitledui.com</p>
-            </div>
-
-            <Link className="link-el" to="/">
-              <button className="logout-button" type="button">
-                <img
-                  className="logout-image"
-                  alt="logout"
-                  src="https://hmp.me/d6vk"
-                />
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div>
-          <div className="Transaction-Header-container-1">
-            <h1 className="accounts-text">Transactions</h1>
-          </div>
-
-          <div className="all-transactions-ul-el">
-            <div className="transaction-heading-container">
-              <p className="userName-text ut">UserName</p>
-              <p className="userName-text Transaction-Name">Transaction Name</p>
-              <p className="userName-text Category-text">Category</p>
-              <p className="userName-text date-text">Date</p>
-              <p className="userName-text amount-text">Amount</p>
-            </div>
-            <ul>
-              {allTransactionsDetails.map(eachItem => (
-                <TransactionItem
-                  key={eachItem.id}
-                  transactionDetails={eachItem}
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
+        {this.renderAllTransactionView()}
       </div>
     )
   }
